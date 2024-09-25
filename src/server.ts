c3bf077c4de36f84baa7f80e057ba614344d2f92
Davi Rios <manueldavisilveirarios123@gmail.com>
2024-09-25 20:12:48 -0300

    fix: dois cors permitidos

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: 'http://127.0.0.1:5173',
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
