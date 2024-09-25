b06db75f5022cfb90128b9efce1357cc24db37c4
Davi Rios <manueldavisilveirarios123@gmail.com>
2024-09-25 16:40:49 -0300

    fix mudança porta cross-origin

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
