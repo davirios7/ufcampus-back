c085e7802df3601d7f1c64db61ebc5256f2a9be2
Davi Rios <manueldavisilveirarios123@gmail.com>
2024-09-25 16:55:53 -0300

    fix: mudança nome da chave estrangeira da tarefa

--- src/models/tarefa.model.ts
+++ src/models/tarefa.model.ts
@@ -25,7 +25,7 @@ const TarefaSchema: Schema = new Schema({
     type: Date,
     required: true,
   },
-  id_user: {
+  registration: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
